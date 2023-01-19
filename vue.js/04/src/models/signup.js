4caf2d5dbd6d08c6fcdd27af81fcf24c4269a8a7
Ryu <ryujt658@gmail.com>
2023-01-19 20:14:03 +0900

    Update signup.js

--- vue.js/04/src/models/signup.js
+++ vue.js/04/src/models/signup.js
@@ -4,6 +4,7 @@ export default {
 
         return {
             data: {
+                // TODO: 에러 테스트 할 때는 아래 코드를 사용합니다.
                 // errorCode: 100,
                 // errorMessage: "이미 등록된 이메일입니다.",
             },
